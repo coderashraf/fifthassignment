0780a8bb5a58c9dd88dd54bde4cdd9f10d0cf7e5
coderashraf <ashrafulbnt@gmail.com>
2021-12-30 21:35:18 +0530

    third commit

--- src/Components/StudentsCompo.js
+++ src/Components/StudentsCompo.js
@@ -1,22 +1,24 @@
 // import * as React from 'react';
-import React, { useContext } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { DataContext } from './DataContext';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { DataContext } from "./DataContext";
+import { Link } from "react-router-dom";
 
 const StudentsCompo = () => {
-  const [rows, setRows] = useContext(DataContext)
+  const [rows] = useContext(DataContext);
   return (
-    <div id='studentCompo'>
-      <div className='flex'>
+    <div id="studentCompo">
+      <div className="flex">
         <h1>StudentsCompo</h1>
-        <button><Link to="/student-desc/">Add new student</Link> </button>
+        <button>
+          <Link to="/student-desc/">Add new student</Link>{" "}
+        </button>
       </div>
 
       <TableContainer component={Paper}>
@@ -34,7 +36,7 @@ const StudentsCompo = () => {
             {rows.map((row) => (
               <TableRow
                 key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
                   {row.name}
@@ -42,14 +44,16 @@ const StudentsCompo = () => {
                 <TableCell align="right">{row.age}</TableCell>
                 <TableCell align="right">{row.course}</TableCell>
                 <TableCell align="right">{row.batch}</TableCell>
-                <TableCell align="right"><Link to={`/student-desc/${row.id}`}>Edit</Link></TableCell>
+                <TableCell align="right">
+                  <Link to={`/student-desc/${row.id}`}>Edit</Link>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
     </div>
-  )
-}
+  );
+};
 
-export default StudentsCompo
+export default StudentsCompo;
